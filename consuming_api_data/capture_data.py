ca5e64ff399e84e5d11b439f72dbbdf111a6475f
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-21 23:14:24 +0100

    :rocket:gravando arquivo na landing

---
+++ consuming_api_data/capture_data.py
@@ -0,0 +1,46 @@
+
+import requests
+import json
+from datetime import datetime, timedelta
+
+class CaptureApiData:
+    def __init__(self, url:str):
+        self.url = url
+        self.response = None
+        self.error = None
+
+    def capture_api(self):
+        try:
+            res = requests.get(self.url, timeout=30)
+            res.raise_for_status()
+            self.response = res.json()
+            self.error = 'Nenhum'
+            return self.response
+        
+        except Exception as e:
+            self.erro = str(e)
+            self.response = None
+            return None
+            print(f"Erro na comunicação com a API: {self.error}")
+
+    def save_out(self, path:str, response:str):
+
+        archive = datetime.now() + timedelta(hours=-3)
+        archive = str(archive)
+        archive = archive[0:19]
+
+        path = f'{archive}'
+
+        if response:
+            with open(path,'w', encoding='utf-8') as outputResponse:
+                json.dump(response, outputResponse, indent=4, ensure_ascii=False)
+
+            print(f"Local onde o arquivo foi salvo: '{path}'✍️")
+        else:
+            print(f"Não foi possível salvar. Erro: {self.error}")
+
+    
+
+
+
+        
---
+++ notebook/run.py
@@ -0,0 +1,19 @@
+from consuming_api_data.capture_data import CaptureApiData
+
+
+def executa_pipeline(path):
+    file_path = f'dadosabertos_{path}.json'
+    path = f'/Volumes/workspace/default/landing_zone/{file_path}'
+    print('🔛Iniciando processamento.🔛')
+    print('================================')
+    menager = CaptureApiData(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
+
+
+    resultado = menager.capture_api()
+    print(f'📖Resultado da solicitação:')
+
+    menager.save_out(path, resultado)
+    print(f'✔️Arquivo salvo com sucesso')
+
+if __name__=="__main__":
+    executa_pipeline()   
