589657e84cd730774fbd8797a1362620748c069a
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-21 23:15:36 +0100

    :rocket:gravando arquivo na landing

--- notebook/run.py
+++ notebook/run.py
@@ -3,7 +3,7 @@ from consuming_api_data.capture_data import CaptureApiData
 
 def executa_pipeline(path):
     file_path = f'dadosabertos_{path}.json'
-    path = f'/Volumes/workspace/default/landing_zone/{file_path}'
+    path = f'/Volumes/dev/b_bronze/landing/{file_path}'
     print('🔛Iniciando processamento.🔛')
     print('================================')
     menager = CaptureApiData(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
