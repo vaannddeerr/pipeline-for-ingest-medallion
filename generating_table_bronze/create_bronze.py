dd46e7a1a3ded2981c263daff28f5a00f5a7da99
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-22 02:42:00 +0100

    :wrench:lendo arquivo da landing para gerar a bronze:

--- generating_table_bronze/create_bronze.py
+++ generating_table_bronze/create_bronze.py
@@ -1,12 +1,13 @@
 from pyspark.sql import functions as F
 
 
+
 def create_table_bronze(menager):
     df = (
-           spark
-          .read
-          .format('json')
-          .load(menager.path)
+            menager.spark
+                    .read
+                    .format('json')
+                    .load(menager.path)
           
          )
 
