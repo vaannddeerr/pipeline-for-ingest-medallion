3fa22bbe30d7041a5cdb7e9b57d7e8f50da6a267
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-23 14:33:34 +0100

    :wrench:Criando tabela bronze

--- notebook/run_bronze.py
+++ notebook/run_bronze.py
@@ -1,9 +1,24 @@
 from create_delta_table_bronze.read_landing_for_bronze import CreateTableDelta
 
 def pipeline_bronze():
+    print('Iniciando a leitura para gravação da tabela bronze:')
+    
+    catalog = 'dev'
+    print(f'Catalog(UC):{catalog}')
+
+    schema = 'b_bronze'
+    print(f'Database:{schema}')
+
+    table = 'tb_camara_leg'
+    print(f'Delta Table : {table}')
+
+    tablename = f'{catalog}.{schema}.{table}'
+    print(f' Table Name: {catalog}')
+
+    print('')
     res = CreateTableDelta()
     res.df = res.read_df()
-    res.write_table(res.df)
+    res.write_table(tablename)
 
 if __name__=="__main__":
     pipeline_bronze()
