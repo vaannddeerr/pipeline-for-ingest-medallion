0ed117db88d8f0be9dce7bdc44b87c21e63a2312
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-23 03:28:08 +0100

    :wrench:Criando tabela bronze

--- notebook/run_bronze.py
+++ notebook/run_bronze.py
@@ -2,7 +2,7 @@ from create_delta_table_bronze.read_landing_for_bronze import CreateTableDelta
 
 def pipeline_bronze():
     res = CreateTableDelta()
-    res.read_df()
+    res.df = res.read_df()
     res.write_table()
 
 if __name__=="__main__":
