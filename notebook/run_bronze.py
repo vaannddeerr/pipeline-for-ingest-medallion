a2eba9118f2ee2caced01e75bc1be3a28ea6f641
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-24 21:12:28 +0100

    Ajuste spark_dataset

--- notebook/run_bronze.py
+++ notebook/run_bronze.py
@@ -3,12 +3,13 @@ from dataset_config.spark_dataset import get_path_and_table_name
 
 def pipeline_bronze():
     print('Iniciando a leitura para camada bronze:')
-    tablename,path = get_path_and_table_name(env='dev',layer='bronze')
+    path,tablename = get_path_and_table_name(env='dev',layer='bronze')
     
 
-    print(f'Executa carga para {tablename}')
+    
     res = CreateTableDelta()
     res.df = res.read_df(path)
+    print(f'Executa carga para {tablename}')
     res.write_table(tablename)
 
 if __name__=="__main__":
