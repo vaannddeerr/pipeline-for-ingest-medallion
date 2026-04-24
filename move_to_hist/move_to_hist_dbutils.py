c2f57fa5d37eee42aaabf060b91b8e178b49039b
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-25 00:43:09 +0100

    Ajuste spark_dataset

--- move_to_hist/move_to_hist_dbutils.py
+++ move_to_hist/move_to_hist_dbutils.py
@@ -8,9 +8,10 @@ def move_to_hist():
     dbutils = DBUtils(spark)
 
     path,_ = get_path_and_table_name(env='dev',layer='bronze')
+    path_detino = path[:-43]
     path = path[:-26]
     origem = path +'*'
-    destino = path + 'move_to_hist/'
+    destino = path_detino +'move_to_hist/'
     return dbutils.fs.mv(origem, destino,recurse=True)
 
 move_to_hist()
