21b7884109186add7f8d329b07c6c0e840b16e48
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-25 00:22:33 +0100

    Ajuste spark_dataset

--- move_to_hist/move_to_hist_dbutils.py
+++ move_to_hist/move_to_hist_dbutils.py
@@ -1,11 +1,15 @@
 from pyspark.dbutils import DBUtils
 from commons.spark_config import spark_session
+from dataset_config.spark_dataset import get_path_and_table_name
 
-def move_to_hist(menager):
+
+def move_to_hist():
     spark = spark_session()
     dbutils = DBUtils(spark)
+    
+    path,_ = get_path_and_table_name(env='dev',layer='bronze')
     path = path[:-26]
-    menager.path = path +'*'
-    return dbutils.fs.mv(f'{menager.path}','/Volumes/move_to_hist/',recurse=True)
+    path = path +'*'
+    return dbutils.fs.mv(f'{path}','/Volumes/move_to_hist/',recurse=True)
 
-move_to_hist(menager.path)
+move_to_hist()
