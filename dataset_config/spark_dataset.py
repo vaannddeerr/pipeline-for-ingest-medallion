6695cd433c2bb7c875a97a8fc4fd850bbbca7c05
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-24 20:48:48 +0100

    Ajuste spark_dataset

--- dataset_config/spark_dataset.py
+++ dataset_config/spark_dataset.py
@@ -24,7 +24,7 @@ def get_path_and_table_name(env:str, layer:str):
 
         # 3. Montagem das strings (sem usar o dicionário dentro dos ifs)
         # Exemplo: /volume/dev/b_bronze
-        path = f"/Volume/{env_path}/{prefix}/{folder}"
+        path = f"/Volumes/{env_path}/{prefix}/{folder}"
 
 
         # Exemplo: dev.b_bronze.b_camara_leg
