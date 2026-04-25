7fcf3f9504af52c3f9e8de738bcb41c0d3015e38
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-25 01:38:50 +0100

    Ajuste spark_dataset

--- move_to_hist/move_to_hist_dbutils.py
+++ move_to_hist/move_to_hist_dbutils.py
@@ -1,18 +1,40 @@
 from pyspark.dbutils import DBUtils
 from commons.spark_config import spark_session
 from dataset_config.spark_dataset import get_path_and_table_name
+import os
+
+# Exemplo: o path vem dinamicamente da sua função
+# Ex: '/Volumes/dev/b_bronze/landing/dadosabertos.json'
+path,_ = get_path_and_table_name(env='dev', layer='bronze') 
+
+# 1. Obtenha o diretório onde o arquivo está (o 'landing')
+dir_atual = os.path.dirname(path) 
+
+# 2. Obtenha o diretório pai (o 'b_bronze')
+# Isso garante que você está dentro do volume correto, não importa o nome do volume
+base_volume = os.path.dirname(dir_atual)
+
+# 3. Construa o destino dinamicamente
+# Isso mantém a estrutura: /Volumes/dev/b_bronze/move_to_hist/
+destino = os.path.join(base_volume, 'move_to_hist/')
+
+# Agora seu código é 100% dinâmico e respeita a estrutura do Databricks!
+spark = spark_session()
+dbutils = DBUtils(spark)
+dbutils.fs.mkdirs(destino)
+dbutils.fs.mv(path + '*', destino, recurse=True)
 
 
-def move_to_hist():
-    spark = spark_session()
-    dbutils = DBUtils(spark)
+# def move_to_hist():
+#     spark = spark_session()
+#     dbutils = DBUtils(spark)
 
-    path,_ = get_path_and_table_name(env='dev',layer='bronze')
-    path_detino = path[:-43]
-    path = path[:-26]
-    origem = path +'*'
-    destino = path_detino +'move_to_hist/'
-    dbutils.fs.mkdirs(destino)
-    return dbutils.fs.mv(origem, destino,recurse=True)
+#     path,_ = get_path_and_table_name(env='dev',layer='bronze')
+#     path_destino = path[:-43]
+#     path = path[:-26]
+#     origem = path +'*'
+#     destino = path_destino +'move_to_hist/'
+#     dbutils.fs.mkdirs(destino)
+#     return dbutils.fs.mv(origem, destino,recurse=True)
 
-move_to_hist()
+# move_to_hist()
