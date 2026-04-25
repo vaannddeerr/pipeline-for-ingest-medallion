1c6a3f9609764de3fe44c1897d87d12dcc04f2ba
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-25 02:49:13 +0100

    Ajuste spark_dataset

--- move_to_hist/move_to_hist_dbutils.py
+++ move_to_hist/move_to_hist_dbutils.py
@@ -19,9 +19,9 @@ def move_to_hist():
     arquivos = dbutils.fs.ls(origem)
     
     for arquivo in arquivos:
-        # IMPORTANTE: 
         # 1. Verifica se o nome NÃO é 'move_to_hist/' para evitar mover a pasta de destino pra dentro dela mesma
         # 2. Verifica se o nome NÃO é um diretório de metadados do Spark (como _delta_log ou _temporary)
         if arquivo.name != "move_to_hist/" and not arquivo.name.startswith("_"):
-            dbutils.fs.mv(arquivo.path, destino + arquivo.name)
-            print(f"Arquivo {arquivo.name} movido com sucesso.")
+            if arquivo.name in arquivo.name.endswith(".json"):
+                dbutils.fs.mv(arquivo.path, destino + arquivo.name)
+                print(f"Arquivo {arquivo.name} movido com sucesso.")
