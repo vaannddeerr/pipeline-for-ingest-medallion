ee56dfda82c7e2170d83a31f85fb8c4b51414c87
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-25 02:10:59 +0100

    Ajuste spark_dataset

--- move_to_hist/move_to_hist_dbutils.py
+++ move_to_hist/move_to_hist_dbutils.py
@@ -1,41 +1,17 @@
 from pyspark.dbutils import DBUtils
 from commons.spark_config import spark_session
 from dataset_config.spark_dataset import get_path_and_table_name
-import os
 
-# Exemplo de path: '/Volumes/dev/b_bronze/landing/dadosabertos.json'
-path,_ = get_path_and_table_name(env='dev', layer='bronze')
 
-# 1. Obtém a pasta pai: '/Volumes/dev/b_bronze/landing'
-dir_atual = os.path.dirname(path)
+def move_to_hist():
+    spark = spark_session()
+    dbutils = DBUtils(spark)
 
-# 2. Obtém a base do volume: '/Volumes/dev/b_bronze'
-# Isso garante que estamos dentro do schema 'b_bronze'
-base_volume = os.path.dirname(dir_atual)
+    path,_ = get_path_and_table_name(env='dev',layer='bronze')
+    print(path)
+    destino = '/olumes/dev/b_bronze/move_to_hist/'
+    print(destino)
+    dbutils.fs.mkdirs(destino)
+    return dbutils.fs.mv(origem, destino,recurse=True)
 
-# 3. Define o destino explicitamente dentro do volume b_bronze
-# Isso resulta em: '/Volumes/dev/b_bronze/move_to_hist/'
-destino = os.path.join(base_volume, 'move_to_hist')
-
-# 4. CRÍTICO: Certifique-se de que o caminho termine com '/' para o dbutils
-destino_path = destino + '/'
-
-# 5. Executa
-spark = spark_session()
-dbutils = DBUtils(spark)
-dbutils.fs.mkdirs(destino_path)
-dbutils.fs.mv(path + '*', destino_path, recurse=True)
-
-# def move_to_hist():
-#     spark = spark_session()
-#     dbutils = DBUtils(spark)
-
-#     path,_ = get_path_and_table_name(env='dev',layer='bronze')
-#     path_destino = path[:-43]
-#     path = path[:-26]
-#     origem = path +'*'
-#     destino = path_destino +'move_to_hist/'
-#     dbutils.fs.mkdirs(destino)
-#     return dbutils.fs.mv(origem, destino,recurse=True)
-
-# move_to_hist()
+move_to_hist()
